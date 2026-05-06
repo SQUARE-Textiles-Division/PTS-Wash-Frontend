f0917046ab7c1edc9e82f60d7b1d522be0a81be8
Fahad2240 <fahadish861@gmail.com>
2026-05-06 12:25:44 +0600

    laserbrush&whiskermodifiednewerrorhandlingadded

--- src/components/InProcess/LaserWhikserIn.tsx
+++ src/components/InProcess/LaserWhikserIn.tsx
@@ -316,6 +316,9 @@ export default function LaserWhiskerIn() {
             else if(error.response.data.garment_unit){
               setErrorLog(error.response.data.garment_unit);
             }
+             else if(error.response.data.stage){
+              setErrorLog(error.response.data.stage);
+            }
           }
   
         )
